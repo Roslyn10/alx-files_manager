8037b53dca118d661c46ca4adb627e05c1c5b666
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-10 14:40:56 +0200

    Adding the redis folder, which is a simple interface that interacts with a redis database

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+//Simple interface that reacts with a redis database
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+		this.getAsync = promisify(this.client.get).bind(this.client);
+
+		this.client.on('error', (err) => {
+			console.error('Redis Client Error', err);
+		});
+	}
+	isAlive() {
+		return this.client.connected;
+	}
+	async get(key) {
+		return this.getAsync(key);
+	}
+	async set(key, value, duration) {
+		this.client.setex(key, duration, value);
+	}
+	async del(key) {
+		this.client.del(key);
+	}
+}
+
+
+const redisClient = new RedisClient();
+
+export default redisClient;
