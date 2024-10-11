c93af964aa8752d740d238b2b857b75cc3bc6058
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-11 14:32:43 +0200

    Adding the code for the mango database that contains the DBClient

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,32 @@
 import { MongoClient } from 'mongodb';
 
-class DBClient 
+const host = process.env.DB_HOST || 'locahost';
+const port = process.env.DB_PORT || '27017';
+const database = process.env.DB_DATABASE || 'files_manger';
+
+class DBClient {
+	constructor() {
+		const url = `mongodb://${host}:${port}`;
+		this.client = new MongoClient(url, { userNewUrlParser: true, useUnifiedTopology: true });
+		this.db = null;
+
+		this.client.connect().then(() => {
+			this.db = this.client.db(database);
+		}).catch((err) => {
+			console.error('MongoDB connection error:', err);
+		});
+	}
+
+		isAlive() {
+			return !!this.db;
+		}
+		async nbUSers() {
+			return this.db.collection('users').countDocuments();
+		}
+		async nbFiles() {
+			return this.db.collection('files').countDocuments();
+		}
+}
+
+const dbClient = new DBClient();
+export default dbClient;
