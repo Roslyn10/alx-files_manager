221a5e612aa889d1349e194b8272db0b9c42fc75
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-14 09:43:52 +0200

    Adding the new user and hashing the users password

---
+++ controllers/UsersController.js
@@ -0,0 +1,44 @@
+import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async createUser(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const userExists = await dbClient.db.collection('users').findOne({ email });
+      if (userExists) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPassword = sha1(password);
+      const newUser = {
+        email,
+        password: hashedPassword,
+      };
+
+      const result = await dbClient.db.collection('users').insertOne(newUser);
+
+      return res.status(201).json({
+        id: result.insertedId,
+        email,
+      });
+
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+export default UsersController;
+
