94ff0d1c05d6845567f0e6ec408d0ff4df4c98be
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-14 09:48:26 +0200

    Fixing the semistandard for the db file

--- utils/db.js
+++ utils/db.js
@@ -5,27 +5,29 @@ const port = process.env.DB_PORT || '27017';
 const database = process.env.DB_DATABASE || 'files_manger';
 
 class DBClient {
-	constructor() {
-		const url = `mongodb://${host}:${port}`;
-		this.client = new MongoClient(url, { userNewUrlParser: true, useUnifiedTopology: true });
-		this.db = null;
+  constructor () {
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { userNewUrlParser: true, useUnifiedTopology: true });
+    this.db = null;
 
-		this.client.connect().then(() => {
-			this.db = this.client.db(database);
-		}).catch((err) => {
-			console.error('MongoDB connection error:', err);
-		});
-	}
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
 
-		isAlive() {
-			return !!this.db;
-		}
-		async nbUSers() {
-			return this.db.collection('users').countDocuments();
-		}
-		async nbFiles() {
-			return this.db.collection('files').countDocuments();
-		}
+  isAlive () {
+    return !!this.db;
+  }
+
+  async nbUSers () {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles () {
+    return this.db.collection('files').countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
