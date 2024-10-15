7a6b4179bdbb746338c636627dcada04b18e738d
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-15 21:22:12 +0200

    Adding the rest of the code for the files Controller

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,26 +1,75 @@
-import redicClient from '../utils/redis';
+import { ObjectID } from 'mongodb';
+import { v4 as uuid } from 'uuid';
+
 import dbClient from '../utils/db';
+import { writeFile } from '../utils/files';
+import fileQueue from '../utils/image_upload';
 
-export default class FilesController {
-	static postfiles(req) {
-		const token = req.header('x-token');
-		const rediskey = `auth_${token}`;
-		const userID = await redisClient.get(key);
-		if (userId) {
-			const users = dbClient.db.collection('users');
-			const object_id = new ObjectId(userId);
-			onst user = await users.findOne({ _id: object_id });
-			if(!user) {
-				return null;
-			}
-			return user;
-		}
-		return null;
-	}
-	
-	static async postUpload(req, res) {
-		const user = await FilesCOntroller.getUser(req);
-		if(!user) {
-			return reponse.status(401).json({ error: 'Unauthorized' });
-		}
+const validFileTypes = ['folder', 'file', 'image'];
 
+export const postUpload = async (req, res) => {
+  try {
+    const { name, type, data } = req.body;
+    let { parentId, isPublic } = req.body;
+
+    if (parentId === undefined) parentId = '0';
+    if (isPublic === undefined) isPublic = false;
+
+    if (!name) {
+      res.status(400).json({ error: 'Missing name' });
+      return;
+    }
+
+    if (!type || !validFileTypes.includes(type)) {
+      res.status(400).json({ error: 'Missing type' });
+      return;
+    }
+
+    if (type !== 'folder' && !data) {
+      res.status(400).json({ error: 'Missing data' });
+      return;
+    }
+
+    if (parentId !== '0') {
+      const parent = await dbClient.findFile({ _id: ObjectID(parentId) });
+
+      if (!parent) {
+        res.status(400).json({ error: 'Parent not found' });
+        return;
+      }
+      if (parent.type !== 'folder') {
+        res.status(400).json({ error: 'Parent is not a folder' });
+        return;
+      }
+    }
+
+    const fileData = {
+      userId: req.user.id,
+      name,
+      type,
+      parentId,
+      isPublic,
+    };
+
+    if (type !== 'folder') {
+      fileData.data = data;
+      fileData.path = writeFile(uuid(), type, data);
+    }
+
+    const newFile = await dbClient.uploadFile(fileData);
+
+    if (type === 'image') {
+      await fileQueue.add(newFile);
+    }
+
+    newFile.id = newFile._id;
+    delete newFile._id;
+    delete newFile.data;
+    delete newFile.path;
+
+    res.json(newFile);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
