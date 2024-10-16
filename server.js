7d9a4148ac92b1ae2fecbbabb1e2d1c097f5a0b9
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-16 14:23:54 +0200

    Reverting the server code

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import router from './routes/index';
-
-const port = parseInt(process.env.PORT, 10) || 5000;
+import routeController from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
+routeController(app);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 export default app;
