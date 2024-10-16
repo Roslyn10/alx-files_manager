e59eb16d3518b67e1463a06b0f05a1c6c913115d
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-16 14:06:53 +0200

    Fixing the errors in the Appcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,21 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 export default class AppController {
-  static getStatus (req, res) {
+  static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive()
     });
   }
 
-  static getStats (req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
-  }
-}
+  static async getStats(req, res) {
+    try {
+      const [usersCount, filesCount] = await Promise.all([
+        dbClient.nbUsers(),
+        dbClient.nbFiles()
+      ]);
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error('Error fetching stats:', error);
+      res.status(500)
+
