6f0b48e2e9fc00000a36e14ed6a8e9ab08a46c46
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-16 15:44:03 +0200

    Fixing the code for the authcontroller file

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 class AuthController {
   static async generateAuthToken(req, res) {
@@ -16,7 +16,7 @@ class AuthController {
     const [email, password] = cred.split(':');
 
     const hashedPassword = sha1(password);
-    const collection = await dbClient.userCollection();
+    const collection = await dbClient.db.collection('users');
     const user = await collection.findOne({ email, password: hashedPassword });
 
     if (!user) {
@@ -29,7 +29,23 @@ class AuthController {
 
     return res.status(200).json({ token });
   }
+
+  static async getConnect(req, res) {
+    const token = req.headers['x-Token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Missing token' });
+    }
+
+    const redisKey = `auth_${token}`;
+    const userId = await redisClient.get(redisKey);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    return res.status(200).json({ userId });
+  }
 }
 
 export default AuthController;
-
